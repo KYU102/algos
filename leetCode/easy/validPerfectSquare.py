a87a6382d63e14244e2f15835b4765134c321503
KYU102 <kevinyu25@gmail.com>
2022-07-01 12:30:58 -0700

    test if the number is a perfect square

---
+++ leetCode/easy/validPerfectSquare.py
@@ -0,0 +1,18 @@
+from imghdr import tests
+from operator import truediv
+
+
+def isPerfectSquare(num) -> bool:
+    test = 0
+    testSquared = 0
+    while testSquared < num:
+        testSquared = test*test
+        if testSquared == num:
+            return True
+        test+=1
+    
+    return False
+
+
+
+print(isPerfectSquare(100))

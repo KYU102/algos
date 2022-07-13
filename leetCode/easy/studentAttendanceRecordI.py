b35b67a697bfb31acdc5c59aa25858a9a2d55693
KYU102 <kevinyu25@gmail.com>
2022-07-13 15:46:05 -0700

    update

--- leetCode/easy/studentAttendanceRecordI.py
+++ leetCode/easy/studentAttendanceRecordI.py
@@ -1,17 +1,12 @@
 def checkRecprd (s):
     
-    recordDiction = {
-        'A':'absent',
-        'L':'late',
-        'P':'present'
-    }
     absentCount = 0
     for i in range(len(s)):
         if (s[i] == 'A'):
             absentCount += 1
         if (absentCount == 2):
             return False
-        if (s[i] == 'L' & s[i + 1] == 'L' & s[i + 2] == 'L' ):
+        if (s[i] == 'L' and s[i + 1] == 'L' and s[i + 2] == 'L' ):
             return False
     
     return True

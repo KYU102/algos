a497fd879bd33ddd988e9b9a8e5d234b46a4a6fb
kevinyu <kevinyu25@gmail.com>
2022-07-22 17:06:01 -0700

    init

---
+++ leetCode/easy/decodeTheMessage.py
@@ -0,0 +1,22 @@
+
+def decodeMessage(key, str):
+    key = key.replace(" ","")
+    abcKey = 'abcdefghijklmnopqrstuvwxyz'
+    codeDict = {}
+    for letter in range(26):
+        codeDict[abcKey[letter]] = key[letter]
+
+    index = 0
+    davincisCode = ""
+    
+    while(str[index]):
+        if str[index] == " ":
+            davincisCode += " "
+        else:
+            davincisCode = davincisCode + codeDict[str[index]]
+
+
+    return davincisCode
+
+
+print(decodeMessage("the quick brown fox jumps over the lazy dog", "vkbs bs t suepuv"))

7877dff39c77f51cd711e4638ba2e57954dc8526
kevinyu <kevinyu25@gmail.com>
2022-08-03 11:09:56 -0700

    update

---
+++ leetCode/easy/detectCapital.py
@@ -0,0 +1,21 @@
+
+from cmath import sqrt
+
+
+def capital(word):
+
+    cap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    lower = "abcdefghijklmnopqrstuvwxyz"
+    capDict = {}
+    lowerDict = {}
+
+    for i in cap:
+        capDict[i] = 1
+    for j in lower:
+        lowerDict[j] = 0
+    
+    for letter in word:
+        if 
+
+    return (capDict, lowerDict)
+print(capital(199))

89ee14c5908b3bff1a3ba930b1c8ff9affe07180
kevinyu <kevinyu25@gmail.com>
2022-08-20 16:45:27 -0700

    init

--- leetCode/medium/reduceArraySizeToTheHalf.py
+++ leetCode/medium/reduceArraySizeToTheHalf.py
@@ -1,15 +1,25 @@
 def minSetSize(arr):
     count = {}
+    counter = 0
+    half = len(arr)//2
 
     for num in arr:
         count[num] = count.get(num, 0) + 1
 
     sorted_dict = {}
-    sorted_keys = sorted(count, key=count.get)
+    sorted_keys = sorted(count, key=count.get, reverse=True)
 
     for w in sorted_keys:
         sorted_dict[w] = count[w]
 
-    return sorted_dict
+    for key in count:
+        counter+=1
+        half -= count[key]
+        if half < 1:
+            break
 
-print(minSetSize([3,3,3,3,2,2,7,5,5,5]))
+
+
+    return len(arr) - counter
+
+print(minSetSize([9,77,63,22,92,9,14,54,8,38,18,19,38,68,58,19]))

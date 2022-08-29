8325110b427304962d3361537a36d2800cde90ed
KYU102 <kevinyu25@gmail.com>
2022-08-29 16:02:12 -0700

    init

---
+++ leetCode/easy/backspaceStringCompare.py
@@ -0,0 +1,20 @@
+def backSpaceCompare(s,t):
+
+    sArr = []
+    tArr = []
+
+    for letter in s:
+        if letter == '#' and len(sArr) > 0:
+            sArr.pop()
+        if letter != '#':
+            sArr.append(letter)
+    
+    for letter in t:
+        if letter == '#' and len(tArr) > 0:
+            tArr.pop()
+        if letter != '#':
+            tArr.append(letter)
+
+    return sArr == tArr
+
+print(backSpaceCompare("ab##","c#d#"))

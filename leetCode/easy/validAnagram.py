df9f81522a807dd1ec42b2d2690664f7bc6d43e4
kevinyu <kevinyu25@gmail.com>
2022-09-08 12:47:00 -0700

    update

--- leetCode/easy/validAnagram.py
+++ leetCode/easy/validAnagram.py
@@ -1,13 +1,15 @@
 
-from operator import truediv
 
 
 def isAnagram(s,t):
-    
+
     if len(s) != len(t):
         return False
     
     return sorted(s) == sorted(t)
+
+
+
 
 
 print(isAnagram( "anagram", "nagaram"))

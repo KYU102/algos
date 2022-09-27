57c7db6f9f67219c6c1ac337d0661d73acb15784
KYU102 <kevinyu25@gmail.com>
2022-09-27 10:03:19 -0700

    init

--- leetCode/medium/sortCharactersByFrequency.py
+++ leetCode/medium/sortCharactersByFrequency.py
@@ -1,19 +1,27 @@
 
+from audioop import reverse
 from typing import Counter
 
 
 def frequencySort (s):
     counter = {}
-    sortedCounter = counter.items()
+    res=''
     for letter in s:
         if letter not in counter:
             counter[letter] = counter.get(letter, 1)
         else:
             counter[letter] += 1
 
+    sortedCounter = sorted(counter,key=counter.get,reverse=True)
 
 
-    return sortedCounter
+    for letter in sortedCounter:
+        res+=letter*counter[letter]
+
+
+
+
+    return res
 
 
 print(frequencySort("tree"))
